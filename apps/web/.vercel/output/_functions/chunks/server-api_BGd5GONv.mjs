96b70cad6d40883ce918a9ead602b34ffce9bddc
altrax18 <sabax378@gmail.com>
2026-05-05 12:47:52 +0200

    modified .gitignore

--- apps/web/.vercel/output/_functions/chunks/server-api_BGd5GONv.mjs
+++
@@ -1,6 +0,0 @@
-import { treaty } from '@elysiajs/eden';
-import { a as app } from './index_BDoxk-zr.mjs';
-
-const serverApi = treaty(app);
-
-export { serverApi as s };
--- apps/web/.vercel/output/_functions/entry.mjs
+++
@@ -1,1 +0,0 @@
-export { z as default } from './chunks/entrypoint_DF_D8ei4.mjs';
